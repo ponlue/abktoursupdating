a295968c65386455f2d47a8b1a4df174c0e4d8a0
reachponlue <reachponlue@reachponlue-PC.mshome.net>
2011-04-30 11:04:42 +0700

    commit1

---
+++ administrator/components/com_booking/views/booking/tmpl/default.php
@@ -0,0 +1,7 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+<h1><?php echo $this->greeting; ?></h1>
+<form method="post">
+	<input type="text" />
+    <input type="submit" value="aaaaaaaa" />
+</form>
