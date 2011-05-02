39df4b07ef990ad8a3d9cd88bbeb4e405f8dd076
reachponlue <reachponlue@reachponlue-PC>
2011-05-02 17:39:32 +0700

    -Remove com_booking - update cocm_ttbooking - update sql

--- administrator/components/com_booking/views/booking/tmpl/default.php
+++
@@ -1,7 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<h1><?php echo $this->greeting; ?></h1>
-<form method="post">
-	<input type="text" />
-    <input type="submit" value="aaaaaaaa" />
-</form>
